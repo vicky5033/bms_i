58c7ffba8d7507fb5eb051bb0995e37c4867bf80
vicky5033 <64684198+vicky5033@users.noreply.github.com>
2020-07-03 15:39:13 +0530

    edit 1

--- task/app.js
+++ task/app.js
@@ -1,12 +1,6 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-const expressValidator = require("express-validator");
-const flash = require("connect-flash");
-const session = require("express-session");
-const passport = require("passport");
-const config = require("./config/database");
-const cookieParser = require("cookie-parser");
 
 const app = express();
 // Load View Engine
@@ -18,33 +12,13 @@ app.set("view engine", "pug");
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
-app.use(cookieParser());
+
 
 // Set Public Folder
 app.use(express.static(path.join(__dirname, "public")));
 
-// Express Session Middleware
-app.use(
-  session({
-    secret: "keyboard cat",
-    resave: true,
-    saveUninitialized: true,
-  })
-);
 
-// Express Messages Middleware
-app.use(require("connect-flash")());
-app.use(function (req, res, next) {
-  res.locals.messages = require("express-messages")(req, res);
-  next();
-});
-app.use(expressValidator());
 
-// Passport Config
-require("./config/passport")(passport);
-// Passport Middleware
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.get("*", function (req, res, next) {
   res.locals.user = req.user || null;
@@ -58,7 +32,7 @@ let events = require("./routes/events");
 let users = require("./routes/users");
 
 app.get("/", (req, res) => {
-  res.send('<a href="/users/register" > Click here</a>', );
+  res.send('<a href="/bms" > Click here</a>', );
 });
 app.use("/bms", bms);
 app.use("/movies", movies);
